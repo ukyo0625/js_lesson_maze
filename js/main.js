9aaebab518f8d11761f4f13df4cb7b96060e2a96
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-26 18:40:42 +0900

    add_canvas

--- js/main.js
+++ js/main.js
@@ -2,13 +2,32 @@
 
 (() => {
   class Maze {
+    constructor(canvas) {
+      this.ctx = canvas.getContext('2d');
+      this.data = [
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 1, 1, 1, 1],
+      ];
+    }
 
+    render() {
+      for (let row = 0; row < 5; row++) {
+        for (let col = 0; col < 5; col++) {
+          if (this.data[row][col] === 1) {
+            this.ctx.fillRect(col * 10, row * 10, 10, 10);
+          }
+        }
+      }
+    }
   }
 
   const canvas = document.querySelector('canvas');
   if (typeof canvas.getContext === 'undefined') {
     return;
   } 
-    const maze = new maze(canvas);
+    const maze = new Maze(canvas);
     maze.render();
   })();
